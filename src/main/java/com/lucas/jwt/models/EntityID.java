54da40ff41a5f04a6f505330cefc4fa2ce737197
lucas0headshot <lucas0headshot@gmail.com>
2024-10-04 00:19:25 -0300

    feat: EntityID.java
    Adicionar classe modelo EntityID

---
+++ src/main/java/com/lucas/jwt/models/EntityID.java
@@ -0,0 +1,23 @@
+package com.lucas.jwt.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class EntityId {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
