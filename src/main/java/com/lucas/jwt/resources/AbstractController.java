cebcd92f52eb8e76a6b755fd642937bc3b6619fe
lucas0headshot <lucas0headshot@gmail.com>
2024-10-04 00:21:02 -0300

    feat: AbstractController.java
    Adicionar classe AbstractController.java

---
+++ src/main/java/com/lucas/jwt/resources/AbstractController.java
@@ -0,0 +1,27 @@
+package com.lucas.jwt.resources;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public abstract class AbstractController {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        List<String> collect = ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(p -> ((FieldError) p).getField() + " "
+                        + p.getDefaultMessage())
+                .collect(Collectors.toList());
+        errors.put("erro", collect.toString());
+        return errors;
+    }
+}
