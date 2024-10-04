15a7f81a17a1515b5f5ee0b1ded01b7f27abe469
lucas0headshot <lucas0headshot@gmail.com>
2024-10-04 01:09:00 -0300

    feat: UsuarioRepository

---
+++ src/main/java/com/lucas/jwt/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.lucas.jwt.repositories;
+
+import com.lucas.jwt.models.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
