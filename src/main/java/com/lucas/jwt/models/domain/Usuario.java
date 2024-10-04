da91d79f092c2164c9e34ef85b49d5aff5cfe0e2
lucas0headshot <lucas0headshot@gmail.com>
2024-10-04 02:45:32 -0300

    refactor: Usuario
    Adicionar implementação da interface UserDetails.

--- src/main/java/com/lucas/jwt/models/domain/Usuario.java
+++ src/main/java/com/lucas/jwt/models/domain/Usuario.java
@@ -6,13 +6,36 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
-public class Usuario extends EntityID {
+public class Usuario extends EntityID implements UserDetails {
     private String login;
     private String senha;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
 }
